b0d42b99aff4c9853d8234cf45cff268618d7c24
bstee615 <bstee615@students.bju.edu>
2019-10-09 12:05:30 -0500

    Stubs for IDT

---
+++ src/descriptor_tables/idt.c
@@ -0,0 +1,30 @@
+#include "idt.h"
+
+#define NUM_ISR 256
+idt_entry_t idt[NUM_ISR];
+
+void load_idt_entry(int index, uint32_t offset, uint16_t selector, uint8_t type_attr)
+{
+    idt[index].offset_lo = offset & 0x0000FFFF;
+    idt[index].offset_hi = (offset & 0xFFFF0000) >> 16;
+
+    idt[index].selector = selector;
+    idt[index].type_attr = type_attr;
+    idt[index].zero = 0;
+}
+
+void init_idt()
+{
+    // Load "not present" into all slots of IDT
+    for (int i = 0; i < NUM_ISR; i++)
+    {
+        load_idt_entry(i, 0, 0x8, 0x0E);
+    }
+
+    // TODO: Load some valid entries
+
+    idt_ptr_t idt_ptr;
+    idt_ptr.size = (NUM_ISR * sizeof(idt_entry_t)) - 1;
+    idt_ptr.offset = (uint32_t)&idt;
+    idt_flush((uint32_t)&idt_ptr);
+}
---
+++ src/descriptor_tables/idt.h
@@ -0,0 +1,28 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include <stdint.h>
+
+struct idt_entry
+{
+    uint16_t offset_lo; // offset bits 0..15
+    uint16_t selector;  // a code segment selector in GDT or LDT
+    uint8_t zero;       // unused, set to 0
+    uint8_t type_attr;  // type and attributes, see https://wiki.osdev.org/IDT
+    uint16_t offset_hi; // offset bits 16..31
+}
+__attribute__((packed));
+typedef struct idt_entry idt_entry_t;
+
+struct idt_ptr
+{
+    uint16_t size;
+    uint32_t offset;
+}
+__attribute__((packed));
+typedef struct idt_ptr idt_ptr_t;
+
+extern void idt_flush(uint32_t);
+void init_idt();
+
+#endif
